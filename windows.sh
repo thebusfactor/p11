4b589eb6fe7a6a1bdec2f6925e061d39d7842f48
unknown <scotthbran@ecs.vuw.ac.nz>
2018-05-02 10:36:27 +1200

    Add windows install method

---
+++ windows.sh
@@ -0,0 +1,4 @@
+python -m pip install pipenv
+python -m pipenv install opencv-python
+python -m pipenv install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+python -m pipenv install kivy

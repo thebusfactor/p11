51dc9ff40cd4f9377aff9c45215935f4aa2bb87f
Sean <sean.stevenson93598@gmail.com>
2018-05-02 11:49:02 +1200

    Updated mac install

--- mac.sh
+++ mac.sh
@@ -1,5 +1,5 @@
 pip install pipenv
 pipenv install opencv-python
 brew install pkg-config sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
-pipenv install Cython==0.26.1
-pipenv install kivy
+pipenv install Cython
+pipenv install https://github.com/kivy/kivy/archive/master.zip

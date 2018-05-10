c7453d3f130476af2270b761499e716b1d9e0c07
James Magallanes <jamesmagallanes@gmail.com>
2018-05-11 10:34:18 +1200

    Added greyscale conversion method

--- src/camera/Image.py
+++ src/camera/Image.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 import numpy
 
 
@@ -7,35 +6,47 @@ class Image:
     def __init__(self, text):
         self.text = text
 
-        # self.output = output
-        # self.frame = frame
+    @staticmethod
+    def convert_image_to_grayscale(frame):
+        imgGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return imgGray
 
     @staticmethod
+    def convert_image_to_edge_mask(frame):
+        edge_image = cv2.Canny(frame, 80, 150)
+        return edge_image
+
+    '''
+    Converts image to HSV mask, and returns the masked image as a binary black and
+    white file - white where green is present and detected. 
+    '''
+    @staticmethod
     def detect_green_and_mask_image(frame):
-        # frame = cv2.imread(output)
-        # cv2.imwrite(output, frame)
-        # greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # cv2.imwrite(output, greyscale)
-        # img = cv2.imread(frame, cv2.IMREAD_GRAYSCALE)
-        #for i in range(len(frame))
         lower_bound = numpy.array([33,80,40])
         upper_bound = numpy.array([102,255,255])
         imgHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(imgHSV, lower_bound, upper_bound)
-        cv2.imshow("mask", mask)
-        cv2.imshow("cam", frame)
-        cv2.waitKey(0)
+        return mask
 
+    '''
+    Converts image to HSV mask, and returns the masked image as a binary black and
+    white file - white where yellow is present and detected. 
+    '''
     @staticmethod
     def detect_yellow_and_mask_image(frame):
         lower_bound = numpy.array([18,120,200])
         upper_bound = numpy.array([28,255,255])
         imgHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(imgHSV, lower_bound, upper_bound)
-        cv2.imshow("mask", mask)
-        cv2.imshow("cam", frame)
-        cv2.waitKey(0)
+        # cv2.imshow("mask", mask)
+        # cv2.imshow("cam", frame)
+        # cv2.waitKey(0)
+        return mask
 
+    '''
+    Converts image to HSV mask, and returns the masked image as a binary black and
+    white file - white where red is present and detected. 
+    '''
     @staticmethod
     def detect_red_and_mask_image(frame):
         # lower_bound = numpy.array([80,70,58])
@@ -45,17 +56,5 @@ class Image:
         imgHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         # imgHSV = cv2.cvtColor(numpy.uint8([[[0,0,255]]]), cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(imgHSV, lower_bound, upper_bound)
-        # res = cv2.bitwise_and(frame, frame, mask= mask)
-        # cv2.imshow("res", res)
-        cv2.imshow("mask", mask)
-        cv2.imshow("cam", frame)
-        cv2.waitKey(0)
-
-    @staticmethod
-    def show_image_on_screen(frame):
-        greyscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        i = cv2.imshow("new window", greyscale_frame)
-        print(frame)
-        cv2.waitKey(500)
-        # time.sleep(3)
-        cv2.destroyAllWindows()
+        res = cv2.bitwise_and(frame, frame, mask= mask)
+        return mask

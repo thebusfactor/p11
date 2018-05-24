d34b263fe4e8d8203527e470dc33324daaecffd8
Dylan Kumar <dylan.kumar@icloud.com>
2018-05-25 09:49:11 +1200

    updating UI, adding undo, refactoring code

--- src/ui/view/test.py
+++ src/ui/view/test.py
@@ -1,31 +1,57 @@
+import kivy
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.graphics import *
 
-
 class DrawInput(Widget):
+
     x1 = -1
     y1 = -1
     x2 = -1
     y2 = -1
-    placed = False
-    line = False
 
-    rect = InstructionGroup()
+    line = True
+    rectangle = False
+
+    objects = []
+    undolist = []
+
+    '''
+    Resets all 4 coordinates
+    '''
+    def reset_coordinates(self):
+        self.x1 = -1
+        self.y1 = -1
+        self.x2 = -1
+        self.y2 = -1
+
+    '''
+    Sets draw tool to line and resets all coordinates
+    '''
+    def set_line(self):
+        self.line = True
+        self.rectangle = False
+        self.reset_coordinates()
+
+    '''
+    Sets draw tool to rectangle and resets all coordinates
+    '''
+    def set_rectangle(self):
+        self.rectangle = True
+        self.line = False
+        self.reset_coordinates()
 
     def on_touch_down(self, touch):
+
+        print("1:")
+        print(self.canvas.children)
         print(touch)
-        Rectangle(pos=(100,100), size=(200,200))
+
         with self.canvas:
 
-            # Add a red color
-            Color(1., 0, 0)
-
             if(self.x1 != -1 and self.y1 != -1 and self.x2 != -1 and self.y2 != -1):
-                self.x1 = -1
-                self.y1 = -1
-                self.x2 = -1
-                self.y2 = -1
+                self.reset_coordinates()
 
             if(self.x1 == -1 and self.y1 == -1):
                 self.x1 = touch.x
@@ -33,34 +59,111 @@ class DrawInput(Widget):
             else:
                 self.x2 = touch.x
                 self.y2 = touch.y
-                self.placed = True
 
                 if(self.line):
-                    touch.ud["line"] = Line(points=[self.x1, self.y1, self.x2, self.y2], width=2)
+                    self.draw_line(touch)
                 else:
                     # Add a rectangle
-                    width = abs(self.x2 - self.x1)
-                    height = abs(self.y2 - self.y1)
-                    #Rectangle(pos=(self.x1 - x_diff, self.y1 - y_diff), size=(x_diff, y_diff))
+                    self.draw_rectangle()
 
 
-                    r = None
+    '''
+    Draws line based on current coordinates
+    '''
+    def draw_line(self, touch):
+        Color(1., 0, 0)
+        touch.ud["line"] = Line(points=[self.x1, self.y1, self.x2, self.y2], width=2)
+        lineCoords = [(self.x1, self.y1), (self.x2, self.y2)]
 
-                    if ((self.y1 - self.y2) < 0 and (self.x1 - self.x2) < 0):
-                        r = Rectangle(pos=(self.x1, self.y1), size=(width, height))
-                        self.rect.add(r)
-                    elif ((self.x1 - self.x2) < 0 ):
-                        r = Rectangle(pos=(self.x1, self.y2), size=(width, height))
-                        self.rect.add(r)
-                    elif ((self.y1 - self.y2) < 0 ):
-                        r = Rectangle(pos=(self.x2, self.y1), size=(width, height))
-                        self.rect.add(r)
-                    elif ((self.x1-self.x2) > 0):
-                        r = Rectangle(pos=(self.x2, self.y2), size=(width, height))
-                        self.rect.add(r)
+        gradient = (self.y2 - self.y1) / (self.x2 - self.x1)
+        constant = self.y1 / (gradient * self.x1)
 
-                    #if(r != None):
-                    self.rect.remove(r)
+        downBy = 400
+
+        y3 = self.y1 - downBy
+        x3 = self.x1 - (self.x2 - self.x1)
+
+        y4 = self.y2 - downBy
+        x4 = self.x2 - (self.x2 - self.x1)
+        # (y4 - constant)/gradient
+
+        print("x1={}, y1={}".format(self.x1, self.y1))
+        print("x2={}, y2={}".format(self.x2, self.y2))
+
+        Color(0, 1, 0)
+        # touch.ud["line"] = Line(points=[self.x1, y3, self.x2, y4], width=4)
+
+        # ensure that shape is still within screen bounds
+        if (y3 < 0):
+            y3 = 0
+        if (y4 < 0):
+            y4 = 0
+        if (x3 < 0):
+            x3 = 0
+        if (x4 < 0):
+            x4 = 0
+        touch.ud["line"] = Line(points=[x3, y3, x4, y4], width=2)
+
+    '''
+    Draws rectangle based on current coordinates
+    '''
+    def draw_rectangle(self):
+
+        width = abs(self.x2 - self.x1)
+        height = abs(self.y2 - self.y1)
+
+        Color(1., 0, 0)
+
+        if ((self.y1 - self.y2) < 0 and (self.x1 - self.x2) < 0):
+            Rectangle(pos=(self.x1, self.y1), size=(width, height))
+        elif ((self.x1 - self.x2) < 0):
+            Rectangle(pos=(self.x1, self.y2), size=(width, height))
+        elif ((self.y1 - self.y2) < 0):
+            Rectangle(pos=(self.x2, self.y1), size=(width, height))
+        elif ((self.x1 - self.x2) > 0):
+            Rectangle(pos=(self.x2, self.y2), size=(width, height))
+
+    def undo(self):
+        # item = self.objects.pop(-1)
+        # self.undolist.append(item)
+        print("CHILDREN")
+        print(self.canvas.children)
+
+        obj = None
+        n=2
+        for child in self.canvas.children[::-1]:
+            if isinstance(child, kivy.graphics.vertex_instructions.Line):
+                if(n > 0):
+                    self.canvas.remove(child)
+                    n -= 1
+
+        # self.canvas.remove(obj)
+
+        # self.canvas.remove(self.canvas.children[-1])
+
+        # self.canvas.remove(self.canvas.children[-2])
+
+    '''
+    Returns the 4 rectangle coordinates
+    '''
+    def get_rectangle_coordinates(self):
+        if(self.rectangle):
+            return self.x1, self.y1, self.x2, self.y2
+
+    '''
+    Returns the 4 placed line coordinates, and the other two 4 coordinates of the second line
+    '''
+    def get_line_coordinates(self):
+        if(self.line):
+            return self.x1, self.y1, self.x2, self.y2 #add 4 more
+
+
+    '''
+    Deletes specified object (line or 
+    '''
+    def delete_object(self):
+        #TODO later
+        return None
 
 
     def on_motion(self, etype, motionevent):
@@ -69,21 +172,44 @@ class DrawInput(Widget):
 
     def on_touch_move(self, touch):
         print("HERE")
-        # print(touch)
-        # if(self.x1 != -1 and self.y1 != -1):
-        #     if(not self.placed):
-        #         self.x2 = touch.x
-        #         self.y2 = touch.y
-        #         touch.ud["line"] = Line(points=[self.x1, self.y1, self.x2, self.y2], width=10)
-        # # touch.ud["line"].points += (touch.x, touch.y)
 
     def on_touch_up(self, touch):
         print("RELEASED!", touch)
 
 
+KV = """
+
+BoxLayout:
+    DrawInput:
+        id: widget
+    Button:
+        text: "undo"
+        size_hint: (0.2, 0.1)
+        on_release:
+            widget.undo()
+    Button:
+        text: "redo"
+        size_hint: (0.2, 0.1)
+        on_release:
+            widget.redo()
+    Button:
+        text: "line"
+        size_hint: (0.2, 0.1)
+        on_release:
+            widget.set_line()
+    Button:
+        text: "rectangle"
+        size_hint: (0.2, 0.1)
+        on_release:
+            widget.set_rectangle()
+
+"""
+
 class SimpleKivy4(App):
     def build(self):
-        return DrawInput()
+        # return DrawInput()
+        root = Builder.load_string(KV)
+        return root
 
 
 if __name__ == "__main__":

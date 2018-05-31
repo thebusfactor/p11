b735742d65b4a208a42dbb29f274ef3d0c3b0b49
Kristen <taitkris@vuw.ac.nz>
2018-06-01 11:09:44 +1200

    Deleted capture videos. Created traffic light branch.
    Former-commit-id: be7bfe4caa9c11e8abf313f52f669b51c00eb759

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 import sys
 
 from model.output import *
-#from ui.sample import Gui
+from ui.sample import Gui
 
 
 def main(argv):
     output_specific_number_of_images(1, 0, 200, 200, 400, 400)
     #output_video()
-    #GUI().run()
-    #GUI().stop()
+    GUI().run()
+    GUI().stop()
 
 
 if __name__ == "__main__":
     main(sys.argv)
+
+pass
 

881f734d00ae10af4ba5496afa30d2787b0b8800
unknown <scotthbran@ecs.vuw.ac.nz>
2018-06-02 00:31:32 +1200

    Changed model to meet 10fps requirement
    Former-commit-id: 64aaaa9f4e7a299b408d8faeeebc40ec9a4d624c

--- src/model/model.py
+++ src/model/model.py
@@ -4,14 +4,13 @@ import time
 from model import light
 from model.video import Video, open_video
 
+video = Video("C:/Users/Brandon/Documents/Git Projects/Bus-Factor/src/model/vid.avi")
 
 def start():
-    video = Video("C:/Users/Brandon/Documents/Git Projects/Bus-Factor/src/model/vid.avi")
-
-
     while(True):
         frame = video.get_frame()
         cv2.imwrite("frame.png", frame)
         if light.is_red(frame):
             print("Light is Red!")
-        time.sleep(1)
+        time.sleep(0.1)
+

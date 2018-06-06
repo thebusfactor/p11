6a6cbddd75e15ce51ae13675fb2ca6dded93eaa2
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-06-07 04:09:44 +1200

    Fixed upsidedown frame bug
    Former-commit-id: a7ecff2c63a932b8d77e5ffd727342c4becf5001

--- src/external/clip.py
+++ src/external/clip.py
@@ -3,13 +3,15 @@ import cv2
 from util.double_point import DoublePoint
 
 
-def clip_frame(frame, double_point: DoublePoint):
+def clip_frame(frame, double_point: DoublePoint, res):
     p1 = double_point.point1
     p2 = double_point.point2
-    pad = 50
-    print(p1)
-    print(p2)
-    f = frame[int(p1[1]):int(p2[1]), int(p1[0]):int(p1[0])]
+    x1 = int(p1[0])
+    x2 = int(p2[0])
+    y1 = int(res[1] - p1[1])
+    y2 = int(res[1] - p2[1])
 
-    cv2.imwrite("test.png", f)
-    return frame
+    # WARNING KIVY 0,0 STARTS AT BOTTOM LEFT
+    # CV2 0,0 START AT TOP LEFT (THE NORMAL WAY)
+    f = frame[y2: y1, x1: x2]
+    return f

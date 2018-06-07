7a97d4f6fc8f7a3c36f45acf1aea3ad4ce74bce6
unknown <scotthbran@ecs.vuw.ac.nz>
2018-06-08 11:26:17 +1200

    Update frame texture to work with frames and not video stream
    Former-commit-id: cd44019a0fb83fb5308bf0a5f024fa80392517e4

--- src/ui/view/frame_texture.py
+++ src/ui/view/frame_texture.py
@@ -5,19 +5,20 @@ import cv2
 
 # Credit: https://gist.github.com/ExpandOcean/de261e66949009f44ad2 for texture method
 
+
 class Cv2FrameTexture(Image):
-    def __init__(self, capture, fps, **kwargs):
+    def __init__(self, fps, **kwargs):
         super(Cv2FrameTexture, self).__init__(**kwargs)
-        self.capture = capture
+        self.frame = None
         Clock.schedule_interval(self.update, 1.0 / fps)
 
     def update(self, dt):
-        ret, frame = self.capture.read()
-        if frame is None:
-            self.capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        if ret:
-            flipped_frame = cv2.flip(frame, 0)
+        if self.frame is not None:
+            flipped_frame = cv2.flip(self.frame, 0)
             frame_bytes = flipped_frame.tostring()
-            image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture = Texture.create(size=(self.frame.shape[1], self.frame.shape[0]), colorfmt='bgr')
             image_texture.blit_buffer(frame_bytes, colorfmt='bgr', bufferfmt='ubyte')
             self.texture = image_texture
+
+    def update_frame(self, frame):
+        self.frame = frame

c74562feb745bc65b87022bdd9583b0c98e7c663
unknown <scotthbran@ecs.vuw.ac.nz>
2018-07-30 09:46:07 +1200

    Add darkflow to install

--- windows.sh
+++ windows.sh
@@ -2,3 +2,8 @@ python -m pip install virtualenv
 python -m virtualenv venv
 source venv/Scripts/activate
 pip install -r requirements-windows.txt
+git clone https://github.com/thtrieu/darkflow.git
+cd darkflow
+pip install .
+cd -
+rm -rf darkflow

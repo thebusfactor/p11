4c9287b24e36c1203917a0e5de9d3eda0cc2b7bb
Nickel64 <snellgnich@ecs.vuw.ac.nz>
2018-08-02 10:32:50 +1200

    Set up working classification of images using darkflow classification library. Built utility object "Classification" to store result data. Co-authored-by: scotthbran <scotthbran@ecs.vuw.ac.nz>
    Former-commit-id: 73bb5df806d409c9dd03bf7642e41dfb6f02b9b5 [formerly 53cadf98eebadfb46c7c16b521f80432adb5c674]
    Former-commit-id: a9d1b9c6a4ba22bea497ca36b007ab1a991d7d3e

--- src/model/ai.py
+++ src/model/ai.py
@@ -1,13 +1,27 @@
-import cv2
+import cv2 as cv
 from darkflow.net.build import TFNet
 import matplotlib.pyplot as plt
 import numpy
 
+from util.classification import Classification
 
 options = {"model": "cfg/yolo.cfg", "load": "bin/yolov2.weights", "threshold": 0.1}
 
 tfnet = TFNet(options)
 
-imgcv = cv2.imread("./sample_img/sample_dog.jpg")
-result = tfnet.return_predict(imgcv)
-print(result)
+
+img = cv.imread("bin/Bus1.jpg")
+img = cv.resize(img, (1920, 1080))
+
+results = tfnet.return_predict(img)
+# cv.imshow("result", img)
+# print(result)
+
+for res in results:
+    clf = Classification(res)
+    print(clf)
+
+
+
+
+cv.waitKey(0)

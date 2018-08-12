534c8f982951e3baf5cd9fa2fe49427552b27505
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-08-13 00:21:43 +1200

    Refactor test_ai code to debug_ai
    Former-commit-id: 445cab79e4ae447c321f7082711b96a138e06202

--- src/test/test_ai.py
+++ src/test/test_ai.py
@@ -4,38 +4,5 @@ import cv2 as cv
 
 start_ai()
 frame = cv.imread("Bus3.jpg")
+classify(frame)
 
-img_width = 800
-img_height = 600
-confidence_threshold = 0.15
-
-bus_colour = (0, 191, 255)
-not_bus_colour = (0, 0, 0)
-
-scaled_img = cv.resize(frame, (img_width, img_height))
-
-classification = classify(scaled_img)
-
-print(classification[0].tl)
-
-window_name = 'image'
-cv.namedWindow(window_name, cv.WINDOW_NORMAL)
-
-#check every detected object
-for i in range(0, len(classification)):
-    c = classification[i]
-    #confidence level of detected object has to be above threshold
-    if(c.conf > confidence_threshold):
-        if(c.label == "bus"):
-            cv.rectangle(scaled_img, (c.tl.get('x'), c.tl.get('y')), (c.br.get('x'), c.br.get('y')),bus_colour,2)
-            cv.putText(scaled_img, c.label,
-                       (c.tl.get('x'), c.tl.get('y') + 15), cv.FONT_HERSHEY_SIMPLEX, 0.7, bus_colour, 2)
-        else:
-            cv.rectangle(scaled_img, (c.tl.get('x'), c.tl.get('y')), (c.br.get('x'), c.br.get('y')),not_bus_colour,2)
-            cv.putText(scaled_img, c.label,
-                       (c.tl.get('x'), c.tl.get('y') + 15), cv.FONT_HERSHEY_SIMPLEX, 0.7, not_bus_colour, 2)
-
-cv.imshow(window_name,scaled_img)
-cv.resizeWindow(window_name, img_width, img_height)
-cv.waitKey(0)
-cv.destroyAllWindows()
--- src/ui/debug_ui.py
+++ src/ui/debug_ui.py
@@ -4,15 +4,37 @@ import cv2 as cv
 class DebugGUI:
 
     ui_name = "Bus-Factor"
+    classifications = None
+    confidence_threshold = 0.01
     frame = None
+    bus_colour = (0, 191, 255)
+    not_bus_colour = (0, 0, 0)
 
-    def __init__(self, frame):
-        self.frame = frame
-        #self.play()
+    def __init__(self):
+        self.frame = None
+        self.play()
 
     def update_frame(self, frame):
         self.frame = frame
+        self.draw_classifications_on_frame()
         cv.imshow(self.ui_name, self.frame)
+
+    def update_classifications(self, classifications):
+        self.classifications = classifications
+
+    def draw_classifications_on_frame(self):
+        if self.classifications is None or self.frame is None: return
+        # check every detected object
+        for i in range(0, len(self.classifications)):
+            c = self.classifications[i]
+            # confidence level of detected object has to be above threshold
+            if c.conf > self.confidence_threshold:
+                if c.label == "bus":
+                    cv.rectangle(self.frame, (c.tl.get('x'), c.tl.get('y')), (c.br.get('x'), c.br.get('y')), self.bus_colour, 2)
+                    cv.putText(self.frame, c.label,(c.tl.get('x'), c.tl.get('y') + 15), cv.FONT_HERSHEY_SIMPLEX, 0.7, self.bus_colour, 2)
+                else:
+                    cv.rectangle(self.frame, (c.tl.get('x'), c.tl.get('y')), (c.br.get('x'), c.br.get('y')), self.not_bus_colour, 2)
+                    cv.putText(self.frame, c.label, (c.tl.get('x'), c.tl.get('y') + 15), cv.FONT_HERSHEY_SIMPLEX, 0.7, self.not_bus_colour, 2)
 
     def play(self):
         while cv.getWindowProperty(self.ui_name, 0) >= 0:

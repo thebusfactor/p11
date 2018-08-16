4d42e851a87eb158b244e0281bb7c5fda498be30
Nickel64 <snellgnich@ecs.vuw.ac.nz>
2018-08-16 12:21:01 +1200

    Finished OBS integration with live violation counter. Closes #184 Co-authored by: @magalljame <magalljame@ecs.vuw.ac.nz>
    Signed-off-by: Nickel64 <snellgnich@ecs.vuw.ac.nz>
    
    Former-commit-id: 63396845d7422bd4e6a9f0418a29610a6c068caf [formerly 831a96f3ae7ae617f9ecb25a2334a29f7848b7a7]
    Former-commit-id: 1b31ad8d965559bf0c7152e4c77086ec131c917e

---
+++ src/model/bus_counter.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+
+def write_to_file(file_path, text):
+    try:
+        f = open(file_path, 'w')
+        f.write(text)
+    except Exception as e:
+        print(e)
+    finally:
+        f.close()
+
+
+def generate_txt(path, count):
+    file_path = path + "\\bus_counter.txt"
+    write_to_file(file_path, "Traffic Violations: " + str(count))
+
+
+def traffic_violation_detected(count):
+    dir_path = str(Path.home())+  "\\Desktop\\Bus Factor\\Bus-Factor"
+    generate_txt(dir_path, count)
+

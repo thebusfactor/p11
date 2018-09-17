2a46a5a9b41187d2c98c7d4f827a96a72edb13b8
Nickel64 <snellgnich@ecs.vuw.ac.nz>
2018-09-17 12:54:33 +1200

    Started work on test script for traffic light classification. Having issues to do with arcane traffic light code Related to #203 Co-authored by: @scotthbran <scotthbran@ecs.vuw.ac.nz>
    Signed-off-by: Nickel64 <snellgnich@ecs.vuw.ac.nz>
    
    Former-commit-id: 32376fb32e0bc4ba1f73360dd37741ce686e897a

---
+++ src/test/test_traffic_light.py
@@ -0,0 +1,37 @@
+# MIT License
+# Copyright (c) 2018 ENGR301-302-2018 / Project-11
+
+import cv2
+from model.traffic_light import TrafficLight
+
+
+def test_red_classification(light: TrafficLight, image, resolution):
+    return light.check_traffic_light(image, resolution)
+
+
+def test_not_red_classification(light: TrafficLight, image, resolution):
+    return light.check_traffic_light(image, resolution)
+#
+#
+# tl = TrafficLight()
+#
+#
+# frame = cv2.imread("redlight.PNG")
+# res = (56, 83)
+# box = [[],[]]
+# box[0][0] = 0
+# box[0][1] = res[0]
+# box[1][0] = res[1]
+# box[1][1] = 0
+# tl.update_box(box)
+# assert test_red_classification(tl, frame, res)
+#
+# frame = cv2.imread("greenlight.PNG")
+# res = (71, 82)
+# box = [[],[]]
+# box[0][0] = 0
+# box[0][1] = res[0]
+# box[1][0] = res[1]
+# box[1][1] = 0
+# tl.update_box(box)
+# assert test_not_red_classification(frame, res)

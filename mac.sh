b991fda2601019f948e190de642af8657d974b47
Nicholas Snellgrove <snellgnich@ecs.vuw.ac.nz>
2018-10-15 11:32:11 +1300

    Update mac.sh to remove venv references
    Former-commit-id: 500b286f0adc93a09cb23e350e3b13ed2cfe3f23

--- mac.sh
+++ mac.sh
@@ -1,6 +1,3 @@
-pip3 install --user virtualenv
-python3 -m virtualenv venv
-source venv/bin/activate
 pip install -r requirements-mac.txt
 git clone https://github.com/thtrieu/darkflow.git
 cd darkflow
